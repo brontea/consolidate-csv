8ef640e5e041235a066366e2cdeef7933130f7c3
brontea <petr.brzon@gmail.com>
2020-03-06 22:58:44 +0100

    Add write_consolidated_data function

--- process_csv.py
+++ process_csv.py
@@ -18,3 +18,19 @@ def get_unique_headers(pattern="*.csv"):
         unique_headers[header].append(file)
 
     return unique_headers
+
+def write_consolidated_data(unique_headers, group_name="group", suffix=".txt"):
+    """Write consolidated data in groups of files."""
+    groups_count = 0
+    for header, files in unique_headers.items():
+        groups_count += 1
+        with open(group_name + "_" + str(groups_count) + suffix, 'w') as group_fh:
+            group_fh.write(header + "\n")
+            for file in files:
+                with open(file, 'r') as current_file:
+                    first_line = current_file.readline()
+                    rest_of_the_lines = current_file.readlines()
+                group_fh.writelines(rest_of_the_lines)
+
+unique_headers = get_unique_headers()
+write_consolidated_data(unique_headers)
