fb555c9ee1146f57c993e9529a8aa903282c58ba
brontea <petr.brzon@gmail.com>
2020-03-06 22:57:33 +0100

    Add get_unique_headers function

--- process_csv.py
+++ process_csv.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import os, glob
+import os, glob, json
 
 def get_header(file):
     """Get header from file."""
@@ -7,3 +7,14 @@ def get_header(file):
         header = fh.readline().strip()
 
     return header
+
+def get_unique_headers(pattern="*.csv"):
+    """Get unique headers from pattern files"""
+    unique_headers = {}
+    for file in glob.glob(pattern):
+        header = get_header(file)
+        if header not in unique_headers:
+            unique_headers[header] = []
+        unique_headers[header].append(file)
+
+    return unique_headers
